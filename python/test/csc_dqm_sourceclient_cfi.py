426720f4313a1936ea12a45157d6b3686dde644c
Valdas Rapsevicius <sha1-696e89da10bf3b1819e4a75ea8d9d57d29d646f7@cern.ch>
2008-08-14 12:52:56 +0000

    cgf update

--- python/test/csc_dqm_sourceclient_cfi.py
+++ python/test/csc_dqm_sourceclient_cfi.py
@@ -6,16 +6,27 @@ import FWCore.ParameterSet.Config as cms
 #--------------------------
 
 dqmClient = cms.EDFilter("CSCMonitorModule",
+
     FractUpdateKey = cms.untracked.uint32(5),
-    FractUpdateEventFreq = cms.untracked.uint32(10),
+
+    FractUpdateEventFreq = cms.untracked.uint32(1),
+
     BookingFile = cms.FileInPath('DQM/CSCMonitorModule/data/emuDQMBooking.xml'),
+
     InputObjects = cms.untracked.InputTag("source"),
+
     ExaminerMask = cms.untracked.uint32(384562166),
+
     ExaminerOutput = cms.untracked.bool(False),
+
     AddressMask = cms.untracked.vstring(),
+
     ExaminerCRCKey = cms.untracked.uint32(7),
+
     monitorName = cms.untracked.string('CSC'),
+
     hitBookDDU = cms.untracked.bool(True)
+
 )
 
 #--------------------------
@@ -23,24 +34,35 @@ dqmClient = cms.EDFilter("CSCMonitorModule",
 #--------------------------
 
 MessageLogger = cms.Service("MessageLogger",
+
     suppressInfo = cms.untracked.vstring('source'),
+
     cout = cms.untracked.PSet(
+
         threshold = cms.untracked.string('INFO'),
+
         WARNING = cms.untracked.PSet(
             limit = cms.untracked.int32(0)
         ),
+
         noLineBreaks = cms.untracked.bool(False)
+
     ),
+
     detailedInfo = cms.untracked.PSet(
         threshold = cms.untracked.string('INFO')
     ),
+
     critical = cms.untracked.PSet(
         threshold = cms.untracked.string('ERROR')
     ),
+
     debugModules = cms.untracked.vstring('CSCMonitormodule'),
+
     destinations = cms.untracked.vstring('detailedInfo', 
         'critical', 
         'cout')
+
 )
 
 
