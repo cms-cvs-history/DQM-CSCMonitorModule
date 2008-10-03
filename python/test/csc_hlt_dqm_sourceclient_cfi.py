7469902eeb05da393e735c14c0199729d3a28e63
Valdas Rapsevicius <sha1-696e89da10bf3b1819e4a75ea8d9d57d29d646f7@cern.ch>
2008-10-03 23:23:32 +0000

    Beta version of HLT module added, beta version of universal (merged local and global dqm) framework added

---
+++ python/test/csc_hlt_dqm_sourceclient_cfi.py
@@ -0,0 +1,59 @@
+import FWCore.ParameterSet.Config as cms
+
+
+#--------------------------
+# DQM Module
+#--------------------------
+
+dqmClient = cms.EDFilter("CSCHLTMonitorModule",
+  InputObjects = cms.untracked.InputTag("source"),
+  ExaminerMask = cms.untracked.uint32(384562166),
+  ExaminerOutput = cms.untracked.bool(False),
+  ExaminerCRCKey = cms.untracked.uint32(7),
+  monitorName = cms.untracked.string('CSC'),
+  FEDIds = cms.untracked.vuint32(750, 751, 752, 753, 754, 755, 756, 757)
+)
+
+#--------------------------
+# Message Logger
+#--------------------------
+
+MessageLogger = cms.Service("MessageLogger",
+
+  suppressInfo = cms.untracked.vstring('source'),
+  suppressDebug = cms.untracked.vstring('source'),
+  suppressWarning = cms.untracked.vstring('source'),
+
+  cout = cms.untracked.PSet(
+    threshold = cms.untracked.string('INFO'),
+    WARNING = cms.untracked.PSet(
+      limit = cms.untracked.int32(0)
+    ),
+    noLineBreaks = cms.untracked.bool(False)
+  ),
+
+  detailedInfo = cms.untracked.PSet(
+    threshold = cms.untracked.string('INFO')
+  ),
+
+  critical = cms.untracked.PSet(
+    threshold = cms.untracked.string('ERROR')
+  ),
+
+  debug = cms.untracked.PSet(
+    threshold = cms.untracked.string('DEBUG')
+  ),
+
+  debugModules = cms.untracked.vstring('CSCHLTMonitormodule'),
+
+  destinations = cms.untracked.vstring(
+    'debug',
+    'detailedInfo', 
+    'critical', 
+    'cout'
+  )
+
+)
+
+
+
