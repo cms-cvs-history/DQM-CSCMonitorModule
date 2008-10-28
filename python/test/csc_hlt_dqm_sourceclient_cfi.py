9057cdcb93e1a6bad38b810c4534e68e7c0b20bb
Valdas Rapsevicius <sha1-696e89da10bf3b1819e4a75ea8d9d57d29d646f7@cern.ch>
2008-10-28 12:16:57 +0000

    CSCDQM namespace refactoring: renamed classes and files, HLT module updated to match FED ids on X axis, cfs file updates

--- python/test/csc_hlt_dqm_sourceclient_cfi.py
+++ python/test/csc_hlt_dqm_sourceclient_cfi.py
@@ -6,12 +6,13 @@ import FWCore.ParameterSet.Config as cms
 #--------------------------
 
 cscDQMEvF = cms.EDFilter("CSCHLTMonitorModule",
-  InputObjects = cms.untracked.InputTag("source"),
-  ExaminerMask = cms.untracked.uint32(384562166),
+  InputObjects =   cms.untracked.InputTag("source"),
+  ExaminerMask =   cms.untracked.uint32(384562166),
   ExaminerOutput = cms.untracked.bool(False),
   ExaminerCRCKey = cms.untracked.uint32(7),
-  monitorName = cms.untracked.string('CSC'),
-  FEDIds = cms.untracked.vuint32(750, 751, 752, 753, 754, 755, 756, 757)
+  monitorName =    cms.untracked.string('CSC'),
+  FEDIdMin =       cms.untracked.uint32(750),
+  FEDIdMax =       cms.untracked.uint32(757)
 )
 
 #--------------------------
