dc8f6db6bf8a5509f0d77c655172734fc4cc07cc
Valdas Rapsevicius <sha1-696e89da10bf3b1819e4a75ea8d9d57d29d646f7@cern.ch>
2008-11-04 17:30:59 +0000

    fractional histogram update frequency changed from 1 to 100

--- python/test/csc_dqm_sourceclient_cfi.py
+++ python/test/csc_dqm_sourceclient_cfi.py
@@ -9,7 +9,7 @@ dqmClient = cms.EDFilter("CSCMonitorModule",
 
     FractUpdateKey = cms.untracked.uint32(5),
 
-    FractUpdateEventFreq = cms.untracked.uint32(1),
+    FractUpdateEventFreq = cms.untracked.uint32(100),
 
     BookingFile = cms.FileInPath('DQM/CSCMonitorModule/data/emuDQMBooking.xml'),
 
