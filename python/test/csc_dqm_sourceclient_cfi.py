c20191439274606061cd7a27a864bdf35df7e887
Valdas Rapsevicius <sha1-696e89da10bf3b1819e4a75ea8d9d57d29d646f7@cern.ch>
2008-11-12 13:50:35 +0000

    DQM module from DQMOffline/Muon added to online and offline

--- python/test/csc_dqm_sourceclient_cfi.py
+++ python/test/csc_dqm_sourceclient_cfi.py
@@ -6,46 +6,26 @@ import FWCore.ParameterSet.Config as cms
 #--------------------------
 
 dqmClient = cms.EDFilter("CSCMonitorModule",
-
-    FractUpdateKey = cms.untracked.uint32(5),
-
-    FractUpdateEventFreq = cms.untracked.uint32(100),
-
-    BookingFile = cms.FileInPath('DQM/CSCMonitorModule/data/emuDQMBooking.xml'),
-
-    InputObjects = cms.untracked.InputTag("source"),
-
-    ExaminerMask = cms.untracked.uint32(384562166),
-
-    ExaminerOutput = cms.untracked.bool(False),
-
-    AddressMask = cms.untracked.vstring(),
-
-    ExaminerCRCKey = cms.untracked.uint32(7),
-
-    monitorName = cms.untracked.string('CSC'),
-
-    hitBookDDU = cms.untracked.bool(True),
-
-    effParameters = cms.untracked.PSet( 
-
-        threshold_cold   = cms.untracked.double(0.1),
-
-        threshold_hot    = cms.untracked.double(2.0),
-
-        threshold_err    = cms.untracked.double(0.1),
-
-        threshold_nodata = cms.untracked.double(1.0),
-
-        sigfail_cold     = cms.untracked.double(5.0),
-
-        sigfail_hot      = cms.untracked.double(5.0),
-
-        sigfail_err      = cms.untracked.double(5.0),
-
-        sigfail_nodata   = cms.untracked.double(5.0)
-
-    )
+  FractUpdateKey = cms.untracked.uint32(5),
+  FractUpdateEventFreq = cms.untracked.uint32(100),
+  BookingFile = cms.FileInPath('DQM/CSCMonitorModule/data/emuDQMBooking.xml'),
+  InputObjects = cms.untracked.InputTag("source"),
+  ExaminerMask = cms.untracked.uint32(384562166),
+  ExaminerOutput = cms.untracked.bool(False),
+  AddressMask = cms.untracked.vstring(),
+  ExaminerCRCKey = cms.untracked.uint32(7),
+  monitorName = cms.untracked.string('CSC'),
+  hitBookDDU = cms.untracked.bool(True),
+  effParameters = cms.untracked.PSet( 
+    threshold_cold   = cms.untracked.double(0.1),
+    threshold_hot    = cms.untracked.double(2.0),
+    threshold_err    = cms.untracked.double(0.1),
+    threshold_nodata = cms.untracked.double(1.0),
+    sigfail_cold     = cms.untracked.double(5.0),
+    sigfail_hot      = cms.untracked.double(5.0),
+    sigfail_err      = cms.untracked.double(5.0),
+    sigfail_nodata   = cms.untracked.double(5.0)
+  )
 )
 
 #--------------------------
@@ -54,34 +34,30 @@ dqmClient = cms.EDFilter("CSCMonitorModule",
 
 MessageLogger = cms.Service("MessageLogger",
 
-#    suppressInfo = cms.untracked.vstring('source'),
-    suppressInfo = cms.untracked.vstring('*'),
+# suppressInfo = cms.untracked.vstring('source'),
+  suppressInfo = cms.untracked.vstring('*'),
 
-    cout = cms.untracked.PSet(
-
-        threshold = cms.untracked.string('INFO'),
-
-        WARNING = cms.untracked.PSet(
-            limit = cms.untracked.int32(0)
-        ),
-
-        noLineBreaks = cms.untracked.bool(False)
-
+  cout = cms.untracked.PSet(
+    threshold = cms.untracked.string('INFO'),
+    WARNING = cms.untracked.PSet(
+      limit = cms.untracked.int32(0)
     ),
+    noLineBreaks = cms.untracked.bool(False)
+  ),
 
-    detailedInfo = cms.untracked.PSet(
-        threshold = cms.untracked.string('INFO')
-    ),
+  detailedInfo = cms.untracked.PSet(
+    threshold = cms.untracked.string('INFO')
+  ),
 
-    critical = cms.untracked.PSet(
-        threshold = cms.untracked.string('ERROR')
-    ),
+  critical = cms.untracked.PSet(
+    threshold = cms.untracked.string('ERROR')
+  ),
 
-    debugModules = cms.untracked.vstring('CSCMonitormodule'),
+  debugModules = cms.untracked.vstring('CSCMonitormodule'),
 
-#    destinations = cms.untracked.vstring('detailedInfo', 
-#        'critical', 
-#        'cout')
+#  destinations = cms.untracked.vstring('detailedInfo', 
+#    'critical', 
+#    'cout')
 
 )
 
--- python/test/csc_dqm_sourceclient_offline_cff.py
+++ python/test/csc_dqm_sourceclient_offline_cff.py
@@ -1,5 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
 from DQM.CSCMonitorModule.test.csc_dqm_sourceclient_cfi import *
+from DQMOffline.Muon.CSCMonitor_cfi import *
+from Configuration.StandardSequences.MagneticField_cff import *
+from Configuration.StandardSequences.RawToDigi_Data_cff import *
+from Configuration.StandardSequences.Reconstruction_cff import *
 
-cscSources = cms.Sequence(dqmClient)
+csc2DRecHits.readBadChambers = cms.bool(False)
+
+#cscSources = cms.Sequence(dqmClient)
+cscSources = cms.Sequence(muonCSCDigis * csc2DRecHits * cscSegments * cscMonitor * dqmClient)
