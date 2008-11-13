7e80cc2afa434cca076a7317b5c4a4c95db614d4
Valdas Rapsevicius <sha1-696e89da10bf3b1819e4a75ea8d9d57d29d646f7@cern.ch>
2008-11-13 11:29:10 +0000

    Offline module removed from offline

--- python/test/csc_dqm_sourceclient_offline_cff.py
+++ python/test/csc_dqm_sourceclient_offline_cff.py
@@ -1,7 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-from DQMOffline.Muon.CSCMonitor_cfi import *
 from DQM.CSCMonitorModule.test.csc_dqm_sourceclient_cfi import *
 from DQM.CSCMonitorModule.test.csc_hlt_dqm_sourceclient_cfi import *
 
-cscSources = cms.Sequence(cscMonitor * dqmClient * cscDQMEvF)
+cscSources = cms.Sequence(dqmClient * cscDQMEvF)
