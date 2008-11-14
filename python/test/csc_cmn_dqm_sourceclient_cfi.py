cf9d10177035884f778c1b91852a675f11a0aef7
Valdas Rapsevicius <sha1-696e89da10bf3b1819e4a75ea8d9d57d29d646f7@cern.ch>
2008-11-14 16:14:19 +0000

    CSCMonitorModuleCmn added; CSCDQM_Logger and CSCDQM_Exception repaired; CSCDQM_Collection::load repaired

---
+++ python/test/csc_cmn_dqm_sourceclient_cfi.py
@@ -0,0 +1,35 @@
+import FWCore.ParameterSet.Config as cms
+
+#--------------------------
+# DQM Module
+#--------------------------
+
+dqmClient = cms.EDFilter("CSCMonitorModuleCmn",
+
+  FractUpdateKey = cms.untracked.uint32(5),
+  FractUpdateEventFreq = cms.untracked.uint32(100),
+
+  BookingFile = cms.FileInPath('DQM/CSCMonitorModule/data/emuDQMBookingCmn.xml'),
+  InputObjects = cms.untracked.InputTag("source"),
+
+  ExaminerMask = cms.untracked.uint32(384562166),
+  ExaminerOutput = cms.untracked.bool(False),
+  ExaminerCRCKey = cms.untracked.uint32(7),
+
+  hitBookDDU = cms.untracked.bool(True),
+
+  AddressMask = cms.untracked.vstring(),
+
+  effParameters = cms.untracked.PSet( 
+    threshold_cold   = cms.untracked.double(0.1),
+    threshold_hot    = cms.untracked.double(2.0),
+    threshold_err    = cms.untracked.double(0.1),
+    threshold_nodata = cms.untracked.double(1.0),
+    sigfail_cold     = cms.untracked.double(5.0),
+    sigfail_hot      = cms.untracked.double(5.0),
+    sigfail_err      = cms.untracked.double(5.0),
+    sigfail_nodata   = cms.untracked.double(5.0)
+  )
+
+)
+
