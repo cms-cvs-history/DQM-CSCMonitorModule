80a08dd6b3a06214646a19bcab25306ed307301e
Valdas Rapsevicius <sha1-696e89da10bf3b1819e4a75ea8d9d57d29d646f7@cern.ch>
2008-12-09 14:01:04 +0000

    Configuration class updated to handle parameter set (global DQM) and XML files (local DQM)

--- python/test/csc_cmn_dqm_sourceclient_cfi.py
+++ python/test/csc_cmn_dqm_sourceclient_cfi.py
@@ -6,29 +6,35 @@ import FWCore.ParameterSet.Config as cms
 
 dqmCSCClient = cms.EDFilter("CSCMonitorModuleCmn",
 
-  FractUpdateKey = cms.untracked.uint32(5),
-  FractUpdateEventFreq = cms.untracked.uint32(1),
+  EventProcessor = cms.untracked.PSet(
+    BINCHECKER_CRC_ALCT = cms.untracked.bool(True),
+    BINCHECKER_CRC_CLCT = cms.untracked.bool(True),
+    BINCHECKER_CRC_CFEB = cms.untracked.bool(True),
+    BINCHECKER_OUTPUT   = cms.untracked.bool(False),
+    FRAEFF_AUTO_UPDATE  = cms.untracked.bool(True),
+    FRAEFF_SEPARATE_THREAD  = cms.untracked.bool(False),
+    BOOKING_XML_FILE = cms.untracked.string('DQM/CSCMonitorModule/data/emuDQMBookingCmn.xml'),
+    FOLDER_EMU = cms.untracked.string('CSC/Summary/'),
+    FOLDER_DDU = cms.untracked.string('CSC/DDU/'),
+    FOLDER_CSC = cms.untracked.string('CSC/CSC/'),
+    FOLDER_PAR = cms.untracked.string('CSC/EventInfo/reportSummaryContents/'),
+    DDU_CHECK_MASK = cms.untracked.uint32(0xFFFFFFFF ),
+    DDU_BINCHECK_MASK = cms.untracked.uint32(0x02080016),
+    BINCHECK_MASK = cms.untracked.uint32(0xFFFFFFFF),
+    FRAEFF_AUTO_UPDATE_START = cms.untracked.uint32(5),
+    FRAEFF_AUTO_UPDATE_FREQ = cms.untracked.uint32(1),
+    EFF_COLD_THRESHOLD = cms.untracked.double(0.1),
+    EFF_COLD_SIGFAIL = cms.untracked.double(5.0),
+    EFF_HOT_THRESHOLD = cms.untracked.double(0.1),
+    EFF_HOT_SIGFAIL = cms.untracked.double(5.0),
+    EFF_ERR_THRESHOLD = cms.untracked.double(0.1),
+    EFF_ERR_SIGFAIL = cms.untracked.double(5.0),
+    EFF_NODATA_THRESHOLD = cms.untracked.double(0.1),
+    EFF_NODATA_SIGFAIL = cms.untracked.double(5.0)
+  ),
 
+  # Just to check that this file exists
   BookingFile = cms.FileInPath('DQM/CSCMonitorModule/data/emuDQMBookingCmn.xml'),
-
-  ExaminerMask = cms.untracked.uint32(384562166),
-  ExaminerOutput = cms.untracked.bool(False),
-  ExaminerCRCKey = cms.untracked.uint32(7),
-
-  hitBookDDU = cms.untracked.bool(True),
-
-  AddressMask = cms.untracked.vstring(),
-
-  effParameters = cms.untracked.PSet( 
-    threshold_cold   = cms.untracked.double(0.1),
-    threshold_hot    = cms.untracked.double(2.0),
-    threshold_err    = cms.untracked.double(0.1),
-    threshold_nodata = cms.untracked.double(1.0),
-    sigfail_cold     = cms.untracked.double(5.0),
-    sigfail_hot      = cms.untracked.double(5.0),
-    sigfail_err      = cms.untracked.double(5.0),
-    sigfail_nodata   = cms.untracked.double(5.0)
-  )
 
 )
 
