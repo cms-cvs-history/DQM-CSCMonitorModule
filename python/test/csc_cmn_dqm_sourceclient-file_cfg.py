e63e8a61ba58c371aee664a3cc42babb1ed53977
Valdas Rapsevicius <sha1-696e89da10bf3b1819e4a75ea8d9d57d29d646f7@cern.ch>
2009-02-19 13:48:27 +0000

    CSCDQM_Lock changed/simplified

--- python/test/csc_cmn_dqm_sourceclient-file_cfg.py
+++ python/test/csc_cmn_dqm_sourceclient-file_cfg.py
@@ -24,84 +24,9 @@ process.load("DQM.CSCMonitorModule.test.csc_cmn_dqm_sourceclient_cfi")
 
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(50))
 
-process.source = cms.Source("DaqSource",
-    readerPluginName = cms.untracked.string('CSCFileReader'),
-    readerPset = cms.untracked.PSet(
-        firstEvent = cms.untracked.int32(0),
-        RUI01 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI01_Monitor_001.raw'),
-        RUI02 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI02_Monitor_001.raw'),
-        RUI03 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI03_Monitor_001.raw'),
-        RUI04 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI04_Monitor_001.raw'),
-        #RUI05 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI05_Monitor_001.raw'),
-        RUI06 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI06_Monitor_001.raw'),
-        RUI07 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI07_Monitor_001.raw'),
-        RUI08 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI08_Monitor_001.raw'),
-        #RUI09 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI09_Monitor_001.raw'),
-        #RUI10 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI10_Monitor_001.raw'),
-        RUI11 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI11_Monitor_001.raw'),
-        RUI12 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI12_Monitor_001.raw'),
-        #RUI13 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI13_Monitor_001.raw'),
-        #RUI14 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI14_Monitor_001.raw'),
-        RUI15 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI15_Monitor_001.raw'),
-        RUI16 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI16_Monitor_001.raw'),
-        RUI17 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI17_Monitor_001.raw'),
-        RUI18 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI18_Monitor_001.raw'),
-        RUI19 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI19_Monitor_001.raw'),
-        RUI20 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI20_Monitor_001.raw'),
-        RUI21 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI21_Monitor_001.raw'),
-        RUI22 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI22_Monitor_001.raw'),
-        #RUI23 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI23_Monitor_001.raw'),
-        RUI24 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI24_Monitor_001.raw'),
-        RUI25 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI25_Monitor_001.raw'),
-        RUI26 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI26_Monitor_001.raw'),
-        RUI27 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI27_Monitor_001.raw'),
-        #RUI28 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI28_Monitor_001.raw'),
-        #RUI29 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI29_Monitor_001.raw'),
-        RUI30 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI30_Monitor_001.raw'),
-        RUI31 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI31_Monitor_001.raw'),
-        RUI32 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI32_Monitor_001.raw'),
-        #RUI33 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI33_Monitor_001.raw'),
-        RUI34 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI34_Monitor_001.raw'),
-        RUI35 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI35_Monitor_001.raw'),
-        RUI36 = cms.untracked.vstring('/tmp/valdo/csc_00069997_EmuRUI36_Monitor_001.raw'),
-        FED750 = cms.untracked.vstring(
-            'RUI01', 
-            'RUI02', 
-            'RUI03', 
-            'RUI04', 
-            #'RUI05', 
-            'RUI06', 
-            'RUI07', 
-            'RUI08', 
-            #'RUI09', 
-            #'RUI10', 
-            'RUI11', 
-            'RUI12', 
-            #'RUI13', 
-            #'RUI14', 
-            'RUI15', 
-            'RUI16', 
-            'RUI17', 
-            'RUI18', 
-            'RUI19', 
-            'RUI20', 
-            'RUI21', 
-            'RUI22', 
-            #'RUI23', 
-            'RUI24', 
-            'RUI25', 
-            'RUI26', 
-            'RUI27', 
-            #'RUI28', 
-            #'RUI29', 
-            'RUI30', 
-            'RUI31', 
-            'RUI32', 
-            #'RUI33', 
-            'RUI34', 
-            'RUI35', 
-            'RUI36')
-    )
+process.source = cms.Source("PoolSource",
+    fileNames  = cms.untracked.vstring('/store/data/Commissioning08/Cosmics/RAW/v1/000/066/910/8CA64FCF-259F-DD11-B86D-000423D99BF2.root'),
+    skipEvents = cms.untracked.uint32(25900)
 )
 
 #----------------------------
@@ -113,7 +38,6 @@ process.load("DQMServices.Components.DQMEnvironment_cfi")
 
 #process.DQMStore.referenceFileName = '/home/dqmdevlocal/reference/csc_reference.root'
 process.DQMStore.referenceFileName = '/afs/cern.ch/user/v/valdo/data/csc_reference.root'
-#process.DQMStore.referenceFileName = '/nfshome0/valdo/CMSSW_2_1_0/src/DQM/CSCMonitorModule/data/csc_reference.root'
 
 #----------------------------
 # DQM Playback Environment
--- python/test/csc_cmn_dqm_sourceclient_cfi.py
+++ python/test/csc_cmn_dqm_sourceclient_cfi.py
@@ -6,14 +6,20 @@ import FWCore.ParameterSet.Config as cms
 
 dqmCSCClient = cms.EDFilter("CSCMonitorModuleCmn",
 
+  BOOKING_XML_FILE = cms.FileInPath('DQM/CSCMonitorModule/data/emuDQMBookingCmn.xml'),
+
   EventProcessor = cms.untracked.PSet(
+    PROCESS_DDU = cms.untracked.bool(True),
+    PROCESS_CSC = cms.untracked.bool(True),
+    PROCESS_EFF_HISTOS = cms.untracked.bool(True),
+    PROCESS_EFF_PARAMETERS = cms.untracked.bool(True),
     BINCHECKER_CRC_ALCT = cms.untracked.bool(True),
     BINCHECKER_CRC_CLCT = cms.untracked.bool(True),
     BINCHECKER_CRC_CFEB = cms.untracked.bool(True),
+    BINCHECKER_MODE_DDU = cms.untracked.bool(True),
     BINCHECKER_OUTPUT   = cms.untracked.bool(False),
     FRAEFF_AUTO_UPDATE  = cms.untracked.bool(True),
     FRAEFF_SEPARATE_THREAD  = cms.untracked.bool(False),
-    BOOKING_XML_FILE = cms.untracked.string('DQM/CSCMonitorModule/data/emuDQMBookingCmn.xml'),
     FOLDER_EMU = cms.untracked.string('CSC/Summary/'),
     FOLDER_DDU = cms.untracked.string('CSC/DDU/'),
     FOLDER_CSC = cms.untracked.string('CSC/CSC/'),
@@ -22,7 +28,7 @@ dqmCSCClient = cms.EDFilter("CSCMonitorModuleCmn",
     DDU_BINCHECK_MASK = cms.untracked.uint32(0x02080016),
     BINCHECK_MASK = cms.untracked.uint32(0xFFFFFFFF),
     FRAEFF_AUTO_UPDATE_START = cms.untracked.uint32(5),
-    FRAEFF_AUTO_UPDATE_FREQ = cms.untracked.uint32(1),
+    FRAEFF_AUTO_UPDATE_FREQ = cms.untracked.uint32(200),
     EFF_COLD_THRESHOLD = cms.untracked.double(0.1),
     EFF_COLD_SIGFAIL = cms.untracked.double(5.0),
     EFF_HOT_THRESHOLD = cms.untracked.double(0.1),
@@ -30,11 +36,13 @@ dqmCSCClient = cms.EDFilter("CSCMonitorModuleCmn",
     EFF_ERR_THRESHOLD = cms.untracked.double(0.1),
     EFF_ERR_SIGFAIL = cms.untracked.double(5.0),
     EFF_NODATA_THRESHOLD = cms.untracked.double(0.1),
-    EFF_NODATA_SIGFAIL = cms.untracked.double(5.0)
-  ),
-
-  # Just to check that this file exists
-  BookingFile = cms.FileInPath('DQM/CSCMonitorModule/data/emuDQMBookingCmn.xml'),
+    EFF_NODATA_SIGFAIL = cms.untracked.double(5.0),
+    EVENTS_ECHO = cms.untracked.uint32(1000),
+    MO_FILTER = cms.untracked.vstring(
+      '+/^.*$/',
+      '-/^FED.*$/'
+    )
+  )
 
 )
 
