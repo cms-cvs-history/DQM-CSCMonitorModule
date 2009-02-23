f08898270411e8386e4efd3dd34e46ddfae9034f
Valdas Rapsevicius <sha1-696e89da10bf3b1819e4a75ea8d9d57d29d646f7@cern.ch>
2009-02-23 13:45:56 +0000

    Removed outdated (prior CSCDQM) files/modules

--- python/test/csc_hlt_dqm_sourceclient_cfi.py
+++ python/test/csc_hlt_dqm_sourceclient_cfi.py
@@ -4,13 +4,47 @@ import FWCore.ParameterSet.Config as cms
 # DQM Module
 #--------------------------
 
-cscDQMEvF = cms.EDFilter("CSCHLTMonitorModule",
-  InputObjects =   cms.untracked.InputTag("source"),
-  ExaminerMask =   cms.untracked.uint32(384562166),
-  ExaminerOutput = cms.untracked.bool(False),
-  ExaminerCRCKey = cms.untracked.uint32(7),
-  monitorName =    cms.untracked.string('CSC'),
-  FEDIdMin =       cms.untracked.uint32(750),
-  FEDIdMax =       cms.untracked.uint32(757)
+cscDQMEvF = cms.EDFilter("CSCMonitorModule",
+
+  BOOKING_XML_FILE = cms.FileInPath('DQM/CSCMonitorModule/data/emuDQMBooking.xml'),
+
+  EventProcessor = cms.untracked.PSet(
+    PROCESS_DDU = cms.untracked.bool(False),
+    PROCESS_CSC = cms.untracked.bool(False),
+    PROCESS_EFF_HISTOS = cms.untracked.bool(False),
+    PROCESS_EFF_PARAMETERS = cms.untracked.bool(False),
+    BINCHECKER_CRC_ALCT = cms.untracked.bool(True),
+    BINCHECKER_CRC_CLCT = cms.untracked.bool(True),
+    BINCHECKER_CRC_CFEB = cms.untracked.bool(True),
+    BINCHECKER_MODE_DDU = cms.untracked.bool(True),
+    BINCHECKER_OUTPUT   = cms.untracked.bool(False),
+    FRAEFF_AUTO_UPDATE  = cms.untracked.bool(False),
+    FRAEFF_SEPARATE_THREAD  = cms.untracked.bool(False),
+    FOLDER_EMU = cms.untracked.string('CSC/FEDIntegrity/'),
+    FOLDER_DDU = cms.untracked.string(''),
+    FOLDER_CSC = cms.untracked.string(''),
+    FOLDER_PAR = cms.untracked.string(''),
+    DDU_CHECK_MASK = cms.untracked.uint32(0xFFFFFFFF ),
+    DDU_BINCHECK_MASK = cms.untracked.uint32(0x02080016),
+    BINCHECK_MASK = cms.untracked.uint32(0xFFFFFFFF),
+    FRAEFF_AUTO_UPDATE_START = cms.untracked.uint32(5),
+    FRAEFF_AUTO_UPDATE_FREQ = cms.untracked.uint32(200),
+    EFF_COLD_THRESHOLD = cms.untracked.double(0.1),
+    EFF_COLD_SIGFAIL = cms.untracked.double(5.0),
+    EFF_HOT_THRESHOLD = cms.untracked.double(0.1),
+    EFF_HOT_SIGFAIL = cms.untracked.double(5.0),
+    EFF_ERR_THRESHOLD = cms.untracked.double(0.1),
+    EFF_ERR_SIGFAIL = cms.untracked.double(5.0),
+    EFF_NODATA_THRESHOLD = cms.untracked.double(0.1),
+    EFF_NODATA_SIGFAIL = cms.untracked.double(5.0),
+    EVENTS_ECHO = cms.untracked.uint32(1000),
+    MO_FILTER = cms.untracked.vstring(
+      '-/^.*$/',
+      '+/FEDEntries/',
+      '+/FEDFatal/',
+      '+/FEDNonFatal/'
+    )
+  )
+
 )
 
